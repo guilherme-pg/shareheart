57bba09e3f2bddeecd85cfc3e19fbceec7b5bf6d
Bárbara Leimig <barbaraleimigdev@gmail.com>
2024-04-11 23:52:18 +0000

    correcao sw

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,48 @@
-var cacheName = 'pwaTeste+-v1.0';
+var cacheName = "pwaTeste+-v1.0";
 
-self.addEventListener('install', event => {
-
+self.addEventListener("install", (event) => {
   self.skipWaiting();
 
   event.waitUntil(
-    caches.open(cacheName)
-      .then(cache => cache.addAll([
-
-        './index.html',
-        
-        '.images/icons/background.png',
-        '.images/icons/favicon.png',
-        '.images/icons/logo.png',
-        '.images/icons/16.png',
-        '.images/icons/32.png',
-        '.images/icons/40.png',
-        '.images/icons/50.png',
-        '.images/icons/60.png',
-        '.images/icons/80.png',
-        '.images/icons/180.png',
-        '.images/icons/1024.png',
-      ]))
+    caches
+      .open(cacheName)
+      .then((cache) =>
+        cache.addAll([
+          "./index.html",
+          "./css/index.css",
+          "./css/standard.css",
+          "./imgs/shareHeartLogo.png",
+          "./AppIcons/logo.png",
+          "./AppIcons/16.png",
+          "./AppIcons/32.png",
+          "./AppIcons/40.png",
+          "./AppIcons/50.png",
+          "./AppIcons/60.png",
+          "./AppIcons/80.png",
+          "./AppIcons/180.png",
+          "./AppIcons/1024.png",
+        ])
+      )
   );
 });
 
-self.addEventListener('message', function (event) {
-  if (event.data.action === 'skipWaiting') {
+self.addEventListener("message", function (event) {
+  if (event.data.action === "skipWaiting") {
     self.skipWaiting();
   }
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener("fetch", function (event) {
   //Atualizacao internet
-  event.respondWith(async function () {
-    try {
-      return await fetch(event.request);
-    } catch (err) {
-      return caches.match(event.request);
-    }
-  }());
+  event.respondWith(
+    (async function () {
+      try {
+        return await fetch(event.request);
+      } catch (err) {
+        return caches.match(event.request);
+      }
+    })()
+  );
 
   //Atualizacao cache
   /*event.respondWith(
@@ -51,5 +54,4 @@ self.addEventListener('fetch', function (event) {
         return fetch(event.request);
       })
   );*/
-
 });
