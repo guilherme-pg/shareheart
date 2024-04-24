12acd36b818757d4db432627baf99a5b6b38a7c2
BarbaraLeimig <barbaraleimigdev@gmail.com>
2024-04-24 20:53:49 -0300

    consumo api, json, recurso hardware

--- js/map.js
+++ js/map.js
@@ -7,120 +7,88 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
 }).addTo(map);
 
-
-// USER pin RED
-var redIcon = L.icon({
-  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41]
-});
-
-
-
 // Marcador para a localização do usuário
 navigator.geolocation.getCurrentPosition(function (position) {
   var userLat = position.coords.latitude;
   var userLng = position.coords.longitude;
-  L.marker([userLat, userLng], {icon: redIcon})
+  L.marker([userLat, userLng])
     .addTo(map)
     .bindPopup("Minha localização atual!")
     .openPopup();
 });
 
-
-
 // Marcadores para instituições em Recife
-// var points = [
-//   {
-//     name: "Academia para o Desenvolvimento da Educação Brasil",
-//     latlng: [-8.061941635948179, -34.872561149372544],
-//   },
-//   {
-//     name: "Associação Arte e Vida",
-//     latlng: [-8.064922397004764, -34.87334425894304],
-//   },
-//   {
-//     name: "Associação Beneficente Filhas de Santana",
-//     latlng: [-8.034312747181934, -34.901202577910254],
-//   },
-//   {
-//     name: "Círculo do Coração de Pernambuco",
-//     latlng: [-8.033883129323511, -34.87820813558206],
-//   },
-//   {
-//     name: "Associação Da 3ª Idade Da Vila São Miguel",
-//     latlng: [-8.0798366645665, -34.91062370859743],
-//   },
-//   {
-//     name: "Associaçãode de Parkinson de Pernambuco",
-//     latlng: [-8.046472077868632, -34.926947093253844],
-//   },
-//   { name: "Anjos do Poço", latlng: [-8.037620389827598, -34.92303615092564] },
-//   {
-//     name: "Associação Casa do Amor",
-//     latlng: [-8.02503501277099, -34.89549249325384],
-//   },
-//   {
-//     name: "Associação Projeto Casa Da Criança",
-//     latlng: [-8.122240207594238, -34.901075522089734],
-//   },
-//   {
-//     name: "Instituição Filhas de Maria Servas da Caridade",
-//     latlng: [-8.063648424526296, -34.890396606746144],
-//   },
-//   { name: "Lar do Nenen", latlng: [-8.050538172653864, -34.91078795092563] },
-//   {
-//     name: "NACC - Núcleo de Apoio à Criança com Câncer",
-//     latlng: [-8.038083490039801, -34.902505637433336],
-//   },
-//   {
-//     name: "Instituição de Caridade Lar Paulo de Tarso",
-//     latlng: [-8.109090222719505, -34.92381166441795],
-//   },
-//   {
-//     name: "Grupo de Ajuda à Criança Carente com Câncer de Pernambuco (GAC-PE)",
-//     latlng: [-8.0464812474827, -34.886646922089746],
-//   },
-//   {
-//     name: "AACD - Associação de Assistência à Criança Deficiente",
-//     latlng: [-8.071789450961326, -34.893094589846704],
-//   },
-// ];
+var points = [
+  {
+    name: "Academia para o Desenvolvimento da Educação Brasil",
+    latlng: [-8.061941635948179, -34.872561149372544],
+  },
+  {
+    name: "Associação Arte e Vida",
+    latlng: [-8.064922397004764, -34.87334425894304],
+  },
+  {
+    name: "Associação Beneficente Filhas de Santana",
+    latlng: [-8.034312747181934, -34.901202577910254],
+  },
+  {
+    name: "Círculo do Coração de Pernambuco",
+    latlng: [-8.033883129323511, -34.87820813558206],
+  },
+  {
+    name: "Associação Da 3ª Idade Da Vila São Miguel",
+    latlng: [-8.0798366645665, -34.91062370859743],
+  },
+  {
+    name: "Associaçãode de Parkinson de Pernambuco",
+    latlng: [-8.046472077868632, -34.926947093253844],
+  },
+  { name: "Anjos do Poço", latlng: [-8.037620389827598, -34.92303615092564] },
+  {
+    name: "Associação Casa do Amor",
+    latlng: [-8.02503501277099, -34.89549249325384],
+  },
+  {
+    name: "Associação Projeto Casa Da Criança",
+    latlng: [-8.122240207594238, -34.901075522089734],
+  },
+  {
+    name: "Instituição Filhas de Maria Servas da Caridade",
+    latlng: [-8.063648424526296, -34.890396606746144],
+  },
+  { name: "Lar do Nenen", latlng: [-8.050538172653864, -34.91078795092563] },
+  {
+    name: "NACC - Núcleo de Apoio à Criança com Câncer",
+    latlng: [-8.038083490039801, -34.902505637433336],
+  },
+  {
+    name: "Instituição de Caridade Lar Paulo de Tarso",
+    latlng: [-8.109090222719505, -34.92381166441795],
+  },
+  {
+    name: "Grupo de Ajuda à Criança Carente com Câncer de Pernambuco (GAC-PE)",
+    latlng: [-8.0464812474827, -34.886646922089746],
+  },
+  {
+    name: "AACD - Associação de Assistência à Criança Deficiente",
+    latlng: [-8.071789450961326, -34.893094589846704],
+  },
+];
 
-// points.forEach(function (point) {
-//   L.marker(point.latlng).addTo(map).bindPopup(point.name);
-// });
-
-// carregar pontos de interesse de JSON
-fetch('../institutions.json')
-  .then(response => response.json())
-  .then(data => {
-      data.institutions.forEach(function (point) {
-          L.marker([point.latitude, point.longitude]).addTo(map)
-              .bindPopup(point.name);
-      });
-  })
-  .catch(error => console.error('Erro ao caregar os pontos de interesse: ', error));
-
-
+points.forEach(function (point) {
+  L.marker(point.latlng).addTo(map).bindPopup(point.name);
+});
 
 // Atualiza o tamanho do mapa quando a janela for redimensionada
 window.addEventListener("resize", function () {
   map.invalidateSize();
 });
 
-
-
 // Seção para calcular as Instituições mais próximas ao usuário
 document.addEventListener("DOMContentLoaded", () => {
   const getLocationButton = document.getElementById("getLocation");
   const userLocationSpan = document.getElementById("userLocation");
   const institutionDiv = document.getElementById("institution");
-  // const list_institutions = document.getElementById("list_institutions");
 
   getLocationButton.addEventListener("click", () => {
     if ("geolocation" in navigator) {
@@ -150,17 +118,14 @@ document.addEventListener("DOMContentLoaded", () => {
           };
         });
 
-        
         // Classifica as instituições por distância
         distances.sort((a, b) => a.distance - b.distance);
-
 
         // Exibe as instituições mais próximas
         institutionDiv.innerHTML = distances.slice(0, 5)
           .map((item) => {
             const arrivalTime = calculateArrivalTime(item.distance);
             return `
-                    <li>
                       <h2>${item.institution.name}</h2>
                       <p><strong>Endereço:</strong> ${
                         item.institution.address
@@ -174,7 +139,6 @@ document.addEventListener("DOMContentLoaded", () => {
                       <p><strong>Telefone:</strong> <a href="tel:+${
                         item.institution.phone
                       }">${item.institution.phone}</a></p><br>
-                    </li>
                   `;
           })
           .join("");
@@ -183,7 +147,6 @@ document.addEventListener("DOMContentLoaded", () => {
       alert("Geolocalização não está disponível no seu navegador.");
     }
   });
-
 
   // Funções para calcular distância e tempo de chegada
   function calculateDistance(lat1, lon1, lat2, lon2) {
